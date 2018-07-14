940d6daba6485e1592f6f57f640a8122abf79e89
Steven Shi <Cangyuan.Shi@jeppesen.com>
2018-07-14 20:17:57 +0800

    remove unused code

--- run_keras_server.py
+++ run_keras_server.py
@@ -72,21 +72,5 @@ def rotate_by_exif(image):
         return image
 
    
-def prepare_image(image, target):
-    # if the image mode is not RGB, convert it
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-
-    # resize the input image and preprocess it
-    image = image.resize(target)
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = imagenet_utils.preprocess_input(image)
-
-    # return the processed image
-    return image
-
-
-
 if __name__ == "__main__":
     app.run(debug = True)
